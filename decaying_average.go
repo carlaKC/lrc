6211b11805bd68e00e9980ed180888947ec028a8
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-08-01 16:01:48 -0400

    localreputation: add decaying average calculations

---
+++ decaying_average.go
@@ -0,0 +1,66 @@
+package localreputation
+
+import (
+	"math"
+	"time"
+
+	"github.com/lightningnetwork/lnd/clock"
+)
+
+type decayingAverage struct {
+	lastUpdate time.Time
+	value      float64
+	decayRate  float64
+	clock      clock.Clock
+}
+
+// calcualteDecayRate determines the decay rate required for the period over
+// which a decaying average is calculated.
+func calcualteDecayRate(period time.Duration) float64 {
+	return math.Pow(0.5, 2/period.Seconds())
+}
+
+// newDecayingAverage creates a decaying average over the period provided, with
+// a starting value of 0.
+func newDecayingAverage(clock clock.Clock,
+	period time.Duration) *decayingAverage {
+
+	return &decayingAverage{
+		lastUpdate: clock.Now(),
+		value:      0,
+		decayRate:  calcualteDecayRate(period),
+		clock:      clock,
+	}
+}
+
+// update modifies the decaying average's value to reflect its value at the
+// current time. This function *must* be used when reading and writing the
+// average's value.
+func (d *decayingAverage) update() {
+	var (
+		now            = d.clock.Now()
+		lastUpdateDiff = d.lastUpdate.Sub(now).Seconds()
+	)
+
+	// Sanity check that the current time is after our last update.
+	if now.Before(d.lastUpdate) {
+		return
+	}
+
+	d.decayRate = math.Pow(d.value*d.decayRate, lastUpdateDiff)
+}
+
+// getValue updates the decaying average to the present and returns its
+// current value.
+func (d *decayingAverage) getValue() float64 {
+	d.update()
+
+	return d.value
+}
+
+// add updates the decaying average to the present and adds the value provided.
+func (d *decayingAverage) add(value float64) {
+	d.update()
+	d.value += value
+	d.lastUpdate = d.clock.Now()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/carlakc/localreputation
 
 go 1.20
+
+require github.com/lightningnetwork/lnd/clock v1.1.0
+
+require github.com/stretchr/testify v1.8.4 // indirect
