3042305ec635bd58dade8c567f13a517e49a6814
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-07-12 14:57:07 -0400

    target_tracker: improve logging for successful HTLCs

--- target_tracker.go
+++ target_tracker.go
@@ -103,8 +103,9 @@ func (t *targetChannelTracker) ResolveInFlight(htlc *ResolvedHTLC,
 	// Add the fees for the forward to the outgoing channel _if_ the
 	// HTLC was successful.
 	if htlc.Success {
-		t.log.Infof("HTLC successful: adding fees to channel: %v: %v",
-			htlc.OutgoingChannel.ToUint64(),
+		t.log.Infof("HTLC successful (%v(%v) -> %v): adding fees "+
+			"to channel: %v", htlc.IncomingChannel.ToUint64(),
+			htlc.IncomingIndex, htlc.OutgoingChannel.ToUint64(),
 			inFlight.ForwardingFee())
 
 		t.revenue.add(float64(inFlight.ForwardingFee()))
